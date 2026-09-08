79b33c3cd8bd916726d62dfd98157bc2c4cfc298
dev-ashir <techashir167@gmail.com>
2026-09-08 13:04:47 +0500

    chore: set default PORT to 8000 in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
             watch: false,
             max_memory_restart: "500M",
             env: {
+                PORT: 8000,
                 NODE_ENV: "production",
             },
             error_file: "./logs/pm2-error.log",
